ca35a5d790663bb725bf70b9655e368e4ae82d64
Masatoshi Itoh <masatoshi9953@gmail.com>
2014-07-16 18:55:17 -0400

    replacing initialize codes to rebar base ones.

--- src/simpledungeonbb_sup.erl
+++ src/simpledungeonbb_sup.erl
@@ -2,6 +2,9 @@
 
 -behaviour(supervisor).
 
+-include("yaws.hrl").
+-include("yaws_api.hrl").
+
 %% API
 -export([start_link/0]).
 
@@ -16,12 +19,54 @@
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+
+	%% start mnesia db.
+	db:change_schema(),
+	db:start(reset_tables),
+
+	%% start yaws web server.
+	con_yaws:start_yaws(?MODULE),
+	
+	{ok, Pid}.
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    ChildSpec = [notice_mgr(), path_finder(), battle_mgr()],
+    {ok, {{one_for_one, 10, 60},ChildSpec}}.
+
+%% ===================================================================
+%% process definition
+%% ===================================================================
+
+notice_mgr() ->
+    ID = notice_mgr,
+    StartFunc = {notice_mgr, start_link, []},
+    Restart = permanent,
+    Shutdown = brutal_kill,
+    Type = worker,
+    Modules = [notice_mgr],
+    _ChildSpec = {ID, StartFunc, Restart, Shutdown, Type, Modules}.
+
+path_finder() ->
+    ID = path_finder,
+    StartFunc = {path_finder, start_link, []},
+    Restart = permanent,
+    Shutdown = brutal_kill,
+    Type = worker,
+    Modules = [path_finder],
+    _ChildSpec = {ID, StartFunc, Restart, Shutdown, Type, Modules}.
+
+battle_mgr() ->
+    ID = battle_mgr,
+    StartFunc = {battle_mgr, start_link, []},
+    Restart = permanent,
+    Shutdown = brutal_kill,
+    Type = worker,
+    Modules = [battle_mgr],
+    _ChildSpec = {ID, StartFunc, Restart, Shutdown, Type, Modules}.
+
 
